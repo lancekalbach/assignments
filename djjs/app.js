3b47339fc93db7d2be5911a54516d8c5718c6f74
Lance Kalbach <lancekalbach@gmail.com>
2022-09-27 13:22:09 -0600

    Finished moms shopping list

---
+++ djjs/app.js
@@ -0,0 +1,7 @@
+function hover ()
+
+var square = document.getElementsByClassName("square");
+square.element.addEventListener("mouseover", hover);
+
+var background = document.getElementById("back");
+background.backgroundColor ="black";
---
+++ exercises/terminal-practice/dir-two/main.js
@@ -0,0 +1,6 @@
+function greet() {
+alert("Hello World!");
+}
+
+var = document.getElementById("js")
+addEventListener("h1", greet)
